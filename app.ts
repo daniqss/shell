35548f49a3260cedc3f347e0d8b00439ac37656d
daniqss <danielqueijo14@gmail.com>
2024-11-28 18:17:46 +0100

    chore(bar): move bar code to widget/bar/

--- app.ts
+++ app.ts
@@ -1,10 +1,21 @@
 import { App } from "astal/gtk3";
-import style from "./style.scss";
-import Bar from "./widget/bar";
+import style from "./app.scss";
+import Bar from "./widget/bar/bar";
+import Applauncher from "./widget/applauncher/applauncher";
 
 App.start({
+  instanceName: "shell",
   css: style,
   main() {
     App.get_monitors().map(Bar);
+  },
+  requestHandler(req: string, res: (response: any) => void) {
+    const args = req.split(" ");
+    if (args[0] === "applauncher") {
+      Applauncher();
+      res("ok");
+    } else {
+      res("unknown request");
+    }
   },
 });
