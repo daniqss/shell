857d6157d7a29c15d74f886ca9653ae5aea587fb
daniqss <danielqueijo14@gmail.com>
2025-02-04 23:20:00 +0100

    fix(bar): filter spotify from tray

--- widget/bar/bar.tsx
+++ widget/bar/bar.tsx
@@ -177,16 +177,21 @@ export function SysTray(): Gtk.Widget {
   return (
     <box className="SysTray">
       {bind(tray, "items").as((items) =>
-        items.map((item) => (
-          <menubutton
-            tooltipMarkup={bind(item, "tooltipMarkup")}
-            usePopover={false}
-            actionGroup={bind(item, "actionGroup").as((ag) => ["dbusmenu", ag])}
-            menuModel={bind(item, "menuModel")}
-          >
-            <icon gicon={bind(item, "gicon")} />
-          </menubutton>
-        ))
+        items
+          .filter((item) => item.iconName !== "spotify-linux-32")
+          .map((item) => (
+            <menubutton
+              tooltipMarkup={bind(item, "tooltipMarkup")}
+              usePopover={false}
+              actionGroup={bind(item, "actionGroup").as((ag) => [
+                "dbusmenu",
+                ag,
+              ])}
+              menuModel={bind(item, "menuModel")}
+            >
+              <icon gicon={bind(item, "gicon")} />
+            </menubutton>
+          ))
       )}
     </box>
   );
